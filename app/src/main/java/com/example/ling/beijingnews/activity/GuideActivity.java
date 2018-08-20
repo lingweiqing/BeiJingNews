6d5e7414de2ed9cee88faa5115c1f2140a178eee
ling_leanrning <ling1471626451@Gmail.com>
2018-08-20 22:01:40 +0800

    引导页面的点的滑动

--- app/src/main/java/com/example/ling/beijingnews/activity/GuideActivity.java
+++ app/src/main/java/com/example/ling/beijingnews/activity/GuideActivity.java
@@ -1,15 +1,19 @@
 package com.example.ling.beijingnews.activity;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.ling.beijingnews.R;
 
@@ -17,10 +21,13 @@ import java.util.ArrayList;
 
 public class GuideActivity extends Activity {
 
+    private static final String TAG = GuideActivity.class.getSimpleName();
     private ViewPager viewpager;
     private Button btn_start_main;
     private LinearLayout ll_point_group;
     private ArrayList<ImageView> imageviews;
+    private ImageView iv_red_point;
+    private int leftmax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class GuideActivity extends Activity {
         viewpager = (ViewPager)findViewById(R.id.viewpager);
         btn_start_main = (Button)findViewById(R.id.btn_start_main);
         ll_point_group = (LinearLayout)findViewById(R.id.ll_point_group);
+        iv_red_point = (ImageView)findViewById(R.id.iv_red_point);
 
         //准备数据
         int [] ids = new int[]{
@@ -44,6 +52,19 @@ public class GuideActivity extends Activity {
             imageView.setBackgroundResource(ids[i]);
             //添加到集合中
             imageviews.add(imageView);
+            //创建点
+            ImageView point = new ImageView(this);
+            point.setBackgroundResource(R.drawable.point_normal);
+            //单位是像素，需要做适配
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(10,10);
+            if (i!=0){
+                //不包括第0个点，所有的点距离左边有十个像素
+                params.leftMargin=10;
+            }
+            point.setLayoutParams(params);
+            // 添加到线性布局里面
+
+            ll_point_group.addView(point);
         }
         //设置viewPager
         viewpager.setAdapter(new PagerAdapter() {
@@ -85,6 +106,57 @@ public class GuideActivity extends Activity {
             public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
                 //super.destroyItem(container, position, object);
                 container.removeView((View) object);
+            }
+        });
+        
+        iv_red_point.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                
+                //执行不止一次
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    iv_red_point.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                //间距=第1个点距离左边的距离-第0个点距离左边的距离
+                leftmax = ll_point_group.getChildAt(1).getLeft()-ll_point_group.getChildAt(0).getLeft();
+                //得到屏幕滑动的百分比
+                viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                    /**
+                     *
+                     * @param position 当前滑动页面的位置
+                     * @param positionOffset    页面滑动的百分比
+                     * @param positionOffsetPixels  滑动的像素
+                     */
+                    @Override
+                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                        Log.e(TAG,"position=="+position+",positionOffset=="+positionOffset+"positionOffsetPixels"+positionOffsetPixels);
+
+                        int leftmargin = (int) (position*leftmax+(positionOffset*leftmax));
+                        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) iv_red_point.getLayoutParams();
+                        params.leftMargin=leftmargin;
+                        iv_red_point.setLayoutParams(params);
+
+
+                    }
+
+                    /**
+                     *
+                     * @param position 当前被选中的页面的位置
+                     */
+
+                    @Override
+                    public void onPageSelected(int position) {
+
+                    }
+
+                    @Override
+                    public void onPageScrollStateChanged(int state) {
+
+                    }
+                });
+
+
+
             }
         });
     }
